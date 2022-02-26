1a0469c1c291fe8a22a940b7c6b77badb148f9d5
Jeff Lin <jeff.andrew.lin@gmail.com>
2022-02-26 15:47:33 -0500

    Updates

---
+++ precache-manifest.65259e11960bbacabd1c60fdd55d2ee8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "878dbc72414f679d57a256e4b507d2f3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f544ef9fb412a0ff0636",
+    "url": "/static/css/main.b8df2260.chunk.css"
+  },
+  {
+    "revision": "6ecf62a67b830c91565e",
+    "url": "/static/js/2.53e28db4.chunk.js"
+  },
+  {
+    "revision": "f544ef9fb412a0ff0636",
+    "url": "/static/js/main.9fcd0811.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5ff2952ade16a1d914622e9541c8c026",
+    "url": "/static/media/my_photo.5ff2952a.jpg"
+  }
+]);
